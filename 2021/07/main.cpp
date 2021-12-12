06d4de4c67e477bf6ac51690cdda3f1a67900d44
Bryan Abueva <bdabueva@gmail.com>
2021-12-11 19:25:18 -0500

    Day07: part1 solved, part2 added to README

--- 2021/07/main.cpp
+++ 2021/07/main.cpp
@@ -6,11 +6,30 @@
 #include <sstream>
 #include <string>
 
+int FindMinimumMoves(std::vector<int> v)
+{
+    int min_moves = 270000000;
+    int total_moves;
+    // std::pair<int, int> result = std::minmax(v);
+    int min = *std::min_element(v.begin(), v.end());
+    int max = *std::max_element(v.begin(), v.end());
+    for (int i = min; i <= max; i++)
+    {
+        total_moves = 0;
+        for (int x : v)
+        {
+            total_moves += std::abs(x - i);
+        }
+        min_moves = std::min(min_moves, total_moves);
+    }
+    return min_moves;
+}
 
 int main()
 {
-    std::string line;
+    std::string line, position;
     std::stringstream lineStream;
+    std::vector<int> positions;
 
     // read input
     std::ifstream input;
@@ -23,10 +42,12 @@ int main()
             std::string position;
             while (std::getline(lineStream, position, ','))
             {
-                // do something with the positions
+                positions.push_back(stoi(position));
             }
         }
     }
 
     // Part 1: How much fuel must they spend to align to the optimal position?
+    int minimum_moves = FindMinimumMoves(positions);
+    std::cout << "The minimum amount of fuel is: " << minimum_moves << std::endl;
 }
